7f78ee0a61a26907a59d13676b9cfcfe6cf96602
jonas060831 <jonas_sulit2001@yahoo.com>
2019-05-01 00:14:49 -0700

    added ejs

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-hello
+var express = require('express');
+
+
+var PORT = 3000;
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.listen(PORT, function(){
+  console.log('Listening to Port:' + PORT)
+});
