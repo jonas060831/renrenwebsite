270657e67349eed41a48afc1ba2c3c38f632c231
jonas060831 <jonas_sulit2001@yahoo.com>
2019-05-06 09:04:28 -0700

    Bootstrap Template

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 
 var PORT = 3000;
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
 
 app.get('/', function(req, res){
   res.render('index');
